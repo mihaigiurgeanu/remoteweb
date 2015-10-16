5cf0e2863eacd4816f78e1154e9871bf15e68fbc
Andreea <amalia85andreea@gmail.com>
2015-10-16 23:33:19 +0200

    Comenzi GPIO.

--- remoteweb.py
+++ remoteweb.py
@@ -1,4 +1,9 @@
 from bottle import route, run, template, static_file
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setmode(4, GPIO.OUT)
+GPIO.output(4, False)
 
 @route('/hello/<name>')
 def index(name):
@@ -7,10 +12,12 @@ def index(name):
 @route('/actions/on')
 def lights_on():
   print "Lights on!"
+  GPIO.output(4, True)
 
 @route('/actions/off')
 def lights_off():
   print "Ligts off!"
+  GPIO.output(4, False)
 
 
 @route('/static/<filepath:path>')
