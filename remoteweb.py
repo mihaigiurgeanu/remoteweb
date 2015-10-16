859a96244efbcb72be00db8e8dfdfad32255c8e2
Andreea <amalia85andreea@gmail.com>
2015-10-16 23:15:14 +0200

    Procesare comenzi pe server.

--- remoteweb.py
+++ remoteweb.py
@@ -4,6 +4,14 @@ from bottle import route, run, template, static_file
 def index(name):
   return template('<b>Hello {{name}}</b>!', name=name)
 
+@route('/actions/on')
+def lights_on():
+  print "Lights on!"
+
+@route('/actions/off')
+def lights_off():
+  print "Ligts off!"
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
